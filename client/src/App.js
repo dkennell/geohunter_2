e0ed33b8a51fcb64563de8f6e147350f89ddf8e5
David Kennell <davidkennell0@gmail.com>
2017-09-01 13:03:15 -0400

    do a whole bunch of cool stuff, wouldn't you like to know

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
     return (
-
+      <div>
         <p>
           ALCATRAZ IS INDIAN LAND
+          by:
         </p>
+        {this.props.user.name}
+      </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    math: state.math
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {setName: (name) => {
+           dispatch({
+             type: "SET_NAME",
+             payload: name
+            })
+          }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
+import { applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+import { combineReducers } from 'redux'
+
+let userReducer = (state = {name: "Max", age: 36}, action) => {
+	switch (action.type){
+	  case "SET_NAME":
+	    return {
+	    	...state,
+	    	name: action.payload
+	    }
+	   case "SET_AGE":
+	    return {
+	    	...state,
+	    	age: action.payload
+	    }
+	  default:
+	    return state
+	}
+}
+
+let mathReducer = (state = {result: 1}, action) => {
+	switch (action.type){
+	  case "ADD":
+	    let newState = {
+	    	...state,
+	    	result: state + action.payload
+	    }
+	    return newState
+	  default:
+	    return state
+	}
+}
+
+let reducer = combineReducers({user: userReducer, math: mathReducer})
+
+let store = createStore(reducer, {}, applyMiddleware(logger))
+
+store.subscribe(() => console.log("Store subscription - Current State: ", store.getState()))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
